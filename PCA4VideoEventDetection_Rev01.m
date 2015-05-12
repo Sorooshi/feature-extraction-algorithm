bb94bae39382c3667ec18e77a348d2d53932bc19
Sorooshi <sr.shalileh@gmail.com>
2015-05-13 00:54:35 +0430

    first commit

---
+++ PCA4VideoEventDetection_Rev01.m
@@ -0,0 +1,44 @@
+% This Code is created by Soroosh Shalileh to implemet the PCA algorithm
+% for Event Detection in Videos.
+% PCA algorithm Ref No. is[9]
+
+clear all; close all; clear memory; clc;
+
+%% Read and play back the movie file
+% Reading the input video
+Video1 = VideoReader('D:\testing\vids\ira_jump.avi');
+get(Video1);
+
+nFrames = Video1.NumberOfFrames;
+vidHeight = Video1.Height;
+vidWidth = Video1.Width;
+
+% Preallocate the movie structure
+mov(1:nFrames) = ...
+    struct('cdata',zeros(vidHeight,vidWidth, 3,'uint8'),...
+    'colormap',[]);
+
+% Read one frame at a time
+for k = 1 : nFrames
+    mov(k).cdata = read(Video1,k);
+end
+
+% Size a figure based on the video's width and height
+hf = figure;
+set(hf, 'position', [250 250 vidWidth vidHeight])
+
+% Play back the movie once at the video's frame rate
+movie(hf, mov, 1, Video1.FrameRate);
+
+%% Dividing the input video into a single frame
+% Thus we can use each frame as a single input image to apple PCA algorithm
+
+for i = 1 : nFrames
+    SingleFrame(:,:,:,i) = read(Video1,i);
+    SingleGrayFrame(:,:,i) = im2double(rgb2gray(SingleFrame(:,:,:,i)));
+    AverageSingleGrayFrame(:,i) = mean(SingleGrayFrame(:,:,i));
+    for j = 1 vidWidth
+    SingleGrayFrameScaled(:,i) = SingleGrayFrame(:,j);
+end
+
+
